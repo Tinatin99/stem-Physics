985b0b03acad5e94e5d2542512ad15bf228f85d8
Tinatin99 <tinatindavitadze7@gmail.com>
2025-02-03 03:39:01 +0400

    add skript.js

---
+++ script.js
@@ -0,0 +1,27 @@
+// var userName = "Nino";
+// console.log(userName);
+var age = 14;
+// console. log(age);
+// var iz_boy = false;
+// console.log(iz_boy);
+
+
+// let userName ="Kakha";
+// console.log(userName);
+
+// userName="Toma";
+// console.log(userName);
+
+const userName ='Nikolas-Sinan';
+console.log(userName);
+
+// userName =' Misha';
+const box = null;
+let box1;
+
+console.log(typeof userName);
+console.log(typeof age);
+
+
+
+
